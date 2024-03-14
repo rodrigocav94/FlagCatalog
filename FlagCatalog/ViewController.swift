90ad18460a68cefe7aa346f0025c360f9bd4596e
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-14 04:15:59 -0300

    Initial Commit

---
+++ FlagCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagCatalog
+//
+//  Created by Rodrigo Cavalcanti on 14/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
